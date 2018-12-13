b9ab5469ad2af7ad9a73474682e332914afc7f1e
Swift.Sun <sunjiwei@yeah.net>
2018-12-13 10:05:46 +0800

    Update constants.js

--- src/mod/constants.js
+++ src/mod/constants.js
@@ -1,5 +1,5 @@
 // api
-const DOMAIN = location.href.indexOf('localhost') > -1 ? '//10.121.177.139:8888' : location.href.indexOf('test') > -1 ? '//10.121.177.139:8000' : '//10.121.177.139:8000'
+const DOMAIN = location.href.indexOf('localhost') > -1 ? '//10.142.141.166:8888' : location.href.indexOf('test') > -1 ? '//10.121.177.139:8000' : '//10.121.177.139:8000'
 
-export const FETCH_TAMPLATE_INFO_URL = `${DOMAIN}/api/template`
-export const SUBMIT_TAMPLATE_INFO_URL = `${DOMAIN}/api/submit`
+export const FETCH_TAMPLATE_INFO_URL = `${DOMAIN}/meepo_test_get`
+export const SUBMIT_TAMPLATE_INFO_URL = `${DOMAIN}/meepo_test_push`
